09cc35fbf726bca15dc1b4cea77551ed0103a963
DomenicoCaliendo <domenico.caliendo@openeconomics.eu>
2026-03-30 23:53:32 +0200

    Add compatibility with vite16

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Le Cercle Tennis Club')</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-50 text-gray-900 min-h-screen">
 
